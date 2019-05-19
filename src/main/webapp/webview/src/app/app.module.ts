33bfd0e0a1314b5c6501c876e7af1ad5a20c9b17
Kezikov Boris <boris200898@icloud.com>
2019-05-19 13:43:40 +0300

    -

--- src/main/webapp/webview/src/app/app.module.ts
+++ src/main/webapp/webview/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataModelComponent } from './data-model/data-model.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataModelComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/main/webapp/webview/src/app/data-model/data-model.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-model',
+  templateUrl: './data-model.component.html',
+  styleUrls: ['./data-model.component.css']
+})
+export class DataModelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
